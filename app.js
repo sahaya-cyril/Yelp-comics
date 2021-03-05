3754adfd4d4a070cbe1b2a4b93143a20f1049e16
Cyril <sahayacyril2015@gmail.com>
2021-03-06 01:43:12 +0530

    Initialize server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("landing");
+});
 
 app.listen("3000", () => {
     console.log("yelp_comic is running...");
